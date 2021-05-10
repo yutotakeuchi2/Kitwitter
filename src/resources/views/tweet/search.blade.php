a658bcf02425eff817fe9d8b11920e4151fea1e7
FOishi <f.oishi@oplan.co.jp>
2021-05-10 15:17:12 +0900

    kitwitter-logoのリダイレクト先変更　　divのclass属性にcontainaerを追加

--- src/resources/views/tweet/search.blade.php
+++ src/resources/views/tweet/search.blade.php
@@ -7,25 +7,27 @@
 
     <h1 align="center">検索結果</h1>
 
-    <div class="row justify-content-center">
-        <div class="col-md-8" >
-            <div class="card">
-                <div class="card-header">検索結果</div>
-                    <div class="card-body" id="time-line">
-                        @foreach($searchResults as $searchResult)
-                            <div class="tweet-line">
-                                <p class="username-font">User : {{$searchResult->user->name}}</p>
-                                <p>{{$searchResult->text}}</p>
-                                    @if(isset($searchResult->content_url))
-                                        @if ($searchResult->content_extension == "image")
-                                            <img src="{{ asset('storage/tweetimage/' . $searchResult->content_url) }}" class="image-size">
-                                        @elseif ({$searchResult->content_extension == "video")
-                                            <video src="{{ asset('storage/tweetimage/' . $searchResult->content_url)}}" controls playsinline controlsList="nodownload"  class="image-size"></video>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8" >
+                <div class="card">
+                    <div class="card-header">検索結果</div>
+                        <div class="card-body" id="time-line">
+                            @foreach($searchResults as $searchResult)
+                                <div class="tweet-line">
+                                    <p class="username-font">User : {{$searchResult->user->name}}</p>
+                                    <p>{{$searchResult->text}}</p>
+                                        @if(isset($searchResult->content_url))
+                                            @if ($searchResult->content_extension == "image")
+                                                <img src="{{ asset('storage/tweetimage/' . $searchResult->content_url) }}" class="image-size">
+                                            @elseif ($searchResult->content_extension == "video")
+                                                <video src="{{ asset('storage/tweetimage/' . $searchResult->content_url)}}" controls playsinline controlsList="nodownload"  class="image-size"></video>
+                                            @endif
                                         @endif
-                                    @endif
-                                <p class="delete"><a href="/destroy/{{$searchResult->id}}">削除</a></p>
-                            </div>
-                        @endforeach
+                                    <p class="delete"><a href="/destroy/{{$searchResult->id}}">削除</a></p>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
