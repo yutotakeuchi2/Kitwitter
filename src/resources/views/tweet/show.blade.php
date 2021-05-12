6ad9671d40f19e1617786b2b5a429dcdaa6fb8c9
FOishi <f.oishi@oplan.co.jp>
2021-05-12 10:54:48 +0900

    tweet/show.bladeの作成　時間表示を作る　viewにデータを反映

---
+++ src/resources/views/tweet/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="search-wrapper">
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8" >
+                <div class="card">
+                        <div class="card-header">Tweet</div>
+                        <div class="card-body" id="time-line">
+                          <div class="tweet-line">
+                            <p class="username-font">User : {{$tweet_data->name}}</p>
+                              <p>{{$tweet_data->text}}</p>
+                              <p>{{$tweet_data->created_at}}</p>
+                              @if(isset($tweet_data->content_url))
+                                @if ($tweet_data->content_extension == "image")
+                                  <img src="{{ asset('storage/tweetimage/' . $tweet_data->content_url) }}" class="image-size">
+                                @elseif ($tweet_data->content_extension == "video")
+                                  <video src="{{ asset('storage/tweetimage/' . $tweet_data->content_url)}}" controls playsinline controlsList="nodownload"  class="image-size"></video>
+                              @endif
+                              @endif
+                            <p class="delete"><a href="/destroy/{{$tweet_data->id}}">削除</a></p>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
