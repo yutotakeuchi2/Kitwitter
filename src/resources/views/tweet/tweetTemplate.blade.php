2c0051c94a777771595b1915a9ee3e708bb871c7
FOishi <f.oishi@oplan.co.jp>
2021-05-13 17:53:30 +0900

    tweettemplate.blade.phpの作成

---
+++ src/resources/views/tweet/tweetTemplate.blade.php
@@ -0,0 +1,36 @@
+@extends('tweet.index')
+
+
+@section('tweet')
+  <div class="card-body" id="time-line">
+        @foreach($tweets as $tweet)
+
+        <div class="tweetLink" >
+        <div id="{{$tweet->id}}">
+              <div class="tweet-line">
+              <p class="username-font">User : <a href="/users/show/{{$tweet->user_id}}>">{{$tweet->user->name}}</a></p>
+              <p>{{$tweet->text}}</p>
+              @if(isset($tweet->content_url))
+              @if ($tweet->content_extension == "image")
+                    <img src="{{ asset('storage/tweetimage/' . $tweet->content_url) }}" class="image-size">
+              @elseif ($tweet->content_extension == "video")
+                    <video src="{{ asset('storage/tweetimage/' . $tweet->content_url)}}" controls playsinline controlsList="nodownload"  class="image-size"></video>
+              @endif
+              @endif
+              <p class="delete"><a href="/destroy/{{$tweet->id}}">削除</a></p>
+              </div>
+              <script>
+                    jQuery(function($){
+                          $('.tweetLink').css('cursor','pointer');
+                          $('.tweetLink').on('click',function(){
+                                window.location.href= "tweet/show/"+$(this).find('div').attr('id');
+                          });
+                    });
+              </script>
+        </div>
+        </div>
+        @endforeach
+        </div>
+  </div>
+
+@endsection
