235bb78ca914708f0cc8ebadc27c9f4b991ca7fb
yutotakeuchi <y.takeuchi@oplan.co.jp>
2021-05-17 18:03:51 +0900

    controllerを記述

--- src/app/Http/Controllers/FavoriteController.php
+++ src/app/Http/Controllers/FavoriteController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Favorite;
 
 class FavoriteController extends Controller
 {
-
+    public function store(Request $request)
+    {
+        $user_id = Auth::user()->id;
+        $post_id = $request->post_id;//ajaxで入れたデータ
+        Favorite::insertFavorite($user_id, $post_id);
+        $fav_count = Favorite::favoriteCount($post_id);
+        return response()->json($fav_count);
+    }
 }
