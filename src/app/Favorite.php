fa20b0317d5b434edbb3636fb3e48315b6a1f248
FOishi <f.oishi@oplan.co.jp>
2021-05-18 12:01:25 +0900

    modelniの作成(いいねされているかの確認)

--- src/app/Favorite.php
+++ src/app/Favorite.php
@@ -32,5 +32,28 @@ class Favorite extends Model
         return $favorite;
 
     }
+    //すでにいいねされているかの確認
+    public function favoriteExist($id,$tweet_id){
+
+        //Favoriteテーブルのレコードにuser_idとtweet_idが一致するものを取得
+        $exist = Favorite::where('user_id','=','$id')
+                            ->where('tweet_id','=','$tweet_id')
+                            ->get();
+
+        // レコードが存在するなら
+        if(!$exist->isEmpty()){
+            // favoriteテーブルのレコードを削除
+            $exist->delete();
+            // $exist = Favorite::where('tweet_id','$tweet_id')
+            //                 ->where('user_id','$id')
+            //                 ->delete();
+
+        // レコードが存在しないなら
+        } else {
+            $this->insertFavorite();
+        }
+
+        return ;
+    }
 
 }
