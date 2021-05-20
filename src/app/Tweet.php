486023acf68286eef360aaa501cc25b30c2bc0d2
yutotakeuchi <y.takeuchi@oplan.co.jp>
2021-05-20 10:17:07 +0900

    miner change

--- src/app/Tweet.php
+++ src/app/Tweet.php
@@ -36,7 +36,7 @@ class Tweet extends Model
 
     public static function destroyTweet($tweet_id){
         Tweet::destroy($tweet_id);
-        return Tweet::all()->sortByDesc('id');
+        return Tweet::withCount('favorites')->orderBy('id', 'desc')->get();
     }
 
     // モデルで空欄例外処理　コントローラーで必要な要素だけ分解する　一緒にいろいろ送るときのデータ構造が違った？stringにキャストしたら治った臭い　要確認...
